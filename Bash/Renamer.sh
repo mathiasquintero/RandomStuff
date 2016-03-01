912a8f3cf443314b1b507dc409f465a5c092edce
Mathias Quintero <mathias.quintero@tum.de>
2016-03-01 14:12:19 -0600

    Adding Parameters

--- Bash/Renamer.sh
+++ Bash/Renamer.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # Will rename all files in a folder by replacing A with B
 
+replace=$1
+with=$2
 for i in $( ls ); do
   echo item: $i
-  foo=${i//file_assignsubmission_/}
-  echo $foo
+  foo=${i//$replace/$with}
+  echo replaced with: $foo
   mv $i $foo
 done
