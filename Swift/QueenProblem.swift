065b27992be72a55dc236a63a178862692c8c9a7
Mathias Quintero <me@quintero.io>
2017-01-23 21:01:13 +0100

    Counting all the solutions to the queens problem

--- Swift/QueenProblem.swift
+++ Swift/QueenProblem.swift
@@ -1,47 +1,66 @@
 import Foundation
 
-struct Queen {
+protocol ChessPiece {
+    var x: Int { get }
+    var y: Int { get }
+    init(x: Int, y: Int)
+    func canAttack(x: Int, y: Int) -> Bool
+}
+
+extension ChessPiece {
+    
+    func canAttack(_ other: ChessPiece) -> Bool {
+        return canAttack(x: other.x, y: other.y)
+    }
+    
+}
+
+struct Queen: ChessPiece {
     let x: Int
     let y: Int
-
-    func canAttack(_ other: Queen) -> Bool {
-        if other.x == x || other.y == y {
+    
+    func canAttack(x: Int, y: Int) -> Bool {
+        if self.x == x || self.y == y {
             return true
         }
-        if abs(other.x - x) == abs(other.y - y) {
+        if abs(self.x - x) == abs(self.y - y) {
             return true
         }
         return false
     }
 }
 
-extension Collection where Iterator.Element == Queen {
-
+extension Collection where Iterator.Element == ChessPiece {
+    
     func fits(x: Int, y: Int) -> Bool {
-        let queen = Queen(x: x, y: y)
-        return filter { $0.canAttack(queen) }
-            .isEmpty
+        return filter { $0.canAttack(x: x, y: y) }
+                .isEmpty
     }
-
+    
 }
 
-func board(_ current: [Queen] = [], y: Int = 0) -> [Queen]? {
+typealias Board = [ChessPiece]
+
+func boards(_ current: Board = [], y: Int = 0) -> [Board] {
     if current.count == 8 {
-        return current
+        return [current]
     }
-    for x in 0..<8 where current.fits(x: x, y: y) {
-        var copy = current
-        copy.append(Queen(x: x, y: y))
-        if let newBoard = board(copy, y: y + 1) {
-            return newBoard
-        }
-    }
-    return nil
+    return (0..<8)
+            .filter { current.fits(x: $0, y: y) }
+            .reduce([]) { results, x in
+                var current = current
+                current.append(Queen(x: x, y: y))
+                var results = results
+                results.append(contentsOf: boards(current, y: y + 1))
+                return results
+            }
+    
 }
 
-// Task: Find a configuration of 8 Queens in a chess board
+// Task: Find all configurations of 8 Queens in a chess board
 // where no queen can Attack another one
 
-let result = board()?.map { (x: $0.x, y: $0.y) } ?? []
+let result = boards()
 
+print("\(result.count) possibilities:")
 print(result)
