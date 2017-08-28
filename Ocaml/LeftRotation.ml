7a7599836387841b034ed712f2bb981f04b4fff5
Mathias Quintero <me@quintero.io>
2017-08-28 18:04:51 +0200

    Update LeftRotation.ml

--- Ocaml/LeftRotation.ml
+++ Ocaml/LeftRotation.ml
@@ -1,22 +1,14 @@
 open List;;
 open Str;;
 open String;;
-
-let reverse l =
-    let rec helper l a = match l with
-        | x::xs -> helper xs (x::a)
-        | [] -> a
-    in helper l [];;
-
-let (@) l i = reverse (i::(reverse l))
-
+let (@) l i = List.append l [i];;
 let rec shift l n = 
     if n = 0 then 
         l 
     else
         match l with 
         | x::xs -> shift (xs @ x) (n - 1)
-        | [] -> []
+        | [] -> [];;
 
 let numbers string = List.map int_of_string (Str.split (Str.regexp " ") string);;
 let string numbers = String.concat " " (List.map string_of_int numbers);;
@@ -27,3 +19,4 @@ let rec input numbers = match numbers with
 let (_, d) = input (numbers (read_line ()));;
 let shifted = shift (numbers (read_line ())) d;;
 print_string (string shifted);;
+
