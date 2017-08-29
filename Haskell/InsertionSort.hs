8fc0494551798c39be1176d265d765493aafa396
Mathias Quintero <me@quintero.io>
2017-08-29 23:34:05 +0200

    Update InsertionSort.hs

--- Haskell/InsertionSort.hs
+++ Haskell/InsertionSort.hs
@@ -1,3 +1,3 @@
 let insert [] a = [a]; insert (x:xs) a = if a > x then x:(insert xs a) else a:(x:xs)
-let reduce [] a f = a; reduce (x:xs) a f = helper xs (f a x) f
+let reduce [] a f = a; reduce (x:xs) a f = reduce xs (f a x) f
 let sort l = reduce l [] insert
