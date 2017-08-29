ccdab5c9cc23a53b68133d3557b671bf07ee493a
Mathias Quintero <me@quintero.io>
2017-08-29 23:23:47 +0200

    Update InsertionSort.ml

--- Ocaml/InsertionSort.ml
+++ Ocaml/InsertionSort.ml
@@ -1,8 +1,9 @@
+let rec reduce l a f = match l with
+  | h::t -> reduce t (f a h) f
+  | [] -> a
+
 let rec insert i l = match l with
   | [] -> [i]
   | h::t -> if h > i then i::l else h::(insert i t)
 
-let sort l =
-  let rec doit a l = (match l with
-  | [] -> a
-  | x::xs -> doit (insert x a) xs) in doit [] l
+let sort l = reduce l [] insert
