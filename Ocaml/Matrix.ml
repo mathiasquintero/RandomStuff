bd20fc83bf4d164e0c4e3bb3bdae26a8cdf137ef
Mathias Quintero <me@quintero.io>
2017-08-31 05:51:57 +0200

    Adding matrix multiplcation example

--- Ocaml/Matrix.ml
+++ Ocaml/Matrix.ml
@@ -1,36 +1,48 @@
 let rec map l f = match l with
     | h::t -> (f h)::(map t f)
-    | _ -> []
+    | _ -> [];;
 
 let rec reduce l a f = match l with
     | h::t -> reduce t (f a h) f
-    | _ -> a
+    | _ -> a;;
 
 let rec zip x y = match x, y with
     | x::xs, y::ys -> (x, y)::(zip xs ys)
-    | _ -> []
+    | _ -> [];;
 
-let length l = reduce l 0 (fun a _ -> a + 1)
+let length l = reduce l 0 (fun a _ -> a + 1);;
 let width l = match l with
     | h::t -> length h
-    | _ -> 0
+    | _ -> 0;;
 
-let multiplyVectors a b = reduce (zip a b) 0.0 (fun a (x, y) -> a +. x *. y)
+let multiplyVectors a b = reduce (zip a b) 0.0 (fun a (x, y) -> a +. x *. y);;
 
 let rec emptyMatrix l =
     if l = 0 then
         []
     else
-        []::(emptyMatrix (l - 1))
+        []::(emptyMatrix (l - 1));;
 
 let rec (@) x y = match x with
     | h::t -> h::(t @ y)
-    | _ -> y
+    | _ -> y;;
 
-let crossAppend x y = map (zip x y) (fun (a, b) -> a @ [b])
+let crossAppend x y = map (zip x y) (fun (a, b) -> a @ [b]);;
 
-let transposed m = reduce m (emptyMatrix (width m)) crossAppend
+let transposed m = reduce m (emptyMatrix (width m)) crossAppend;;
 
 let multiplyMatrices a b =
     let transposed = transposed b in
-    reduce a [] (fun a b -> a @ [map transposed (fun x -> multiplyVectors x b)])
+    reduce a [] (fun a b -> a @ [map transposed (fun x -> multiplyVectors x b)]);;
+
+let a = [
+    [2.0; 3.0];
+    [4.0; 5.0]
+];;
+
+let b = [
+    [2.0; 3.0];
+    [4.0; 5.0]
+];;
+
+let c = multiplyMatrices a b;;
